8d06755e7a9a75ddd68eed2595dc89f5bfd8c20e
HalitcanEmir <halitcanemir06@gmail.com>
2025-10-02 23:11:33 +0300

    Syntax öğreniyorum

---
+++ src/aracınyakmamiktarı.java
@@ -0,0 +1,15 @@
+public class aracınyakmamiktarı {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Aracınız kilometre kaç kuruş yakıyor(Örnek : 0.4");
+        double kurus = scanner.nextDouble();
+        int km = scanner.nextInt();
+        System.out.println("Toplam ödeminiz gereken tutar :" + (kurus + km)+ "tldir");
+
+
+
+    }
+
+
+
+}
---
+++ src/bmi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bmi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kilonuzu Girin:");
+        int kilo = scanner.nextInt();
+        System.out.print("Boyunuzu Girin: (Örnek : 1.72)");
+        double boy = scanner.nextDouble();
+        double bki = kilo / (boy * boy);
+        System.out.println("Beden kitle indeksiniz : " + bki);
+
+    }
+
+
+}
