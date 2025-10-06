65539b2d604a9d44952fda387c120fa1ff021308
HalitcanEmir <halitcanemir06@gmail.com>
2025-10-06 20:45:21 +0300

    Kalıtım 2

---
+++ src/Hayvan.java
@@ -0,0 +1,28 @@
+public class Hayvan {
+
+    private String isimm;
+
+    private int kilo;
+
+    private int boy;
+
+    private int bacak_sayisi;
+
+    public Hayvan(String isimm, int kilo, int boy, int bacak_sayisi) {
+        this.isimm = isimm;
+        this.kilo = kilo;
+        this.boy = boy;
+        this.bacak_sayisi = bacak_sayisi;
+    }
+    public void yemek_ye() {
+        System.out.println("Hayvan şu anda yemek yiyor");
+    }
+    public void harakete_gec(int hız) {
+
+        System.out.println("Hyavan" + hız + "İle haraket ediyor");
+
+
+    }
+
+
+}
